ae9dee99cf35b5812f88013673cd3d4feb505845
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-27 09:32:03 +0100

    wip  2026-01-27T09:32:03+01:00

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 set -o errexit -o nounset -o pipefail
 
 mkdir -p build
-go build -o build/go-tcp-echo-server go/main.go
 
+# Build and deploy Go version
+go build -o build/go-tcp-echo-server go/main.go
+scp build/go-tcp-echo-server martin@192.168.0.104:/tmp/go-tcp-echo-server
+
+# Build and deploy Rust version
 cargo build --manifest-path rust/Cargo.toml --release
 cp rust/target/release/rust-tcp-echo-server build/
+scp build/rust-tcp-echo-server martin@192.168.0.104:/tmp/rust-tcp-echo-server
