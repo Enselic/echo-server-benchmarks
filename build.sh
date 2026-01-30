51f89c03bb2456dbd607da5662e245569848875d
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-30 13:24:54 +0100

    wip  2026-01-30T13:24:54+01:00

--- build.sh
+++ build.sh
@@ -1,42 +1,35 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail -o xtrace
 
-REMOTE_ADDR=${1:-"martin@192.168.0.104"}
-TOKIO_PORT="${TOKIO_PORT:-9001}"
-GO_PORT="${GO_PORT:-9002}"
-THREADS_PORT="${THREADS_PORT:-9003}"
+SSH_USER_AND_HOST=${1:-"martin@192.168.0.104"}
 
 OUTPUT_DIR="$(pwd)/build"
 mkdir -p $OUTPUT_DIR
 
-for server in \
+PORT_BASE=7000
+
+for server_binary in \
         go-tcp-echo-server \
         rust-tokio-tcp-echo-server \
         rust-threaded-tcp-echo-server \
     ; do
 
     (
-        cd $server
+        cd $server_binary
 
-        ./build.sh $OUTPUT_DIR/$server
+        # Build server binary
+        ./build.sh $OUTPUT_DIR/$server_binary
 
-        ssh $REMOTE_ADDR pkill $server
-        "./$server/build.sh"
-        scp build/$server $REMOTE_ADDR:/tmp/$server
+        # Kill old server if running
+        ssh $SSH_USER_AND_HOST pkill $server_binary
+
+        # Upload server binary
+        scp build/$server_binary $SSH_USER_AND_HOST:/tmp/$server_binary
+
+        # Use a unique port
+        SERVER_PORT=$((PORT_BASE++))
+
+        # Start server in background
+        ssh $SSH_USER_AND_HOST "/tmp/$server_binary ${SERVER_PORT}" &
     )
-
-        
-
-    # Build and deploy Rust tokio version
-    cargo build --manifest-path rust-tokio-server/Cargo.toml --release
-    cp rust-tokio-server/target/release/rust-tcp-echo-server build/
-    scp build/rust-tcp-echo-server $REMOTE_ADDR:/tmp/rust-tcp-echo-server
-
-    # Build and deploy Rust threads version
-    rustc rust-threads-server/main.rs -o build/rust-threads-tcp-echo-server
-    scp build/rust-threads-tcp-echo-server $REMOTE_ADDR:/tmp/rust-threads-tcp-echo-server
-    # Run all three servers on the remote machine
-    ssh $REMOTE_ADDR "/tmp/rust-tcp-echo-server ${TOKIO_PORT}" &
-    ssh $REMOTE_ADDR "/tmp/rust-threads-tcp-echo-server ${THREADS_PORT}" &
-    ssh $REMOTE_ADDR "/tmp/go-tcp-echo-server ${GO_PORT}"
-    
+done
