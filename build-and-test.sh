ee5512195717cddab507b85fb74be43af5016bd1
Martin Nordholts <martin.nordholts@codetale.se>
2026-02-03 20:53:51 +0100

    wip  2026-02-03T20:53:51+01:00

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
+set -o errexit -o nounset -o pipefail -o xtrace
 
 TEST_HOST=${1:-"192.168.0.104"}
 TEST_USER=${2:-"martin"}
-PARALLEL_CLIENTS=${3:-10}
+PARALLEL_CLIENTS=${3:-100}
+
+# todo explain
+trap 'kill -- -$$' EXIT
 
 SSH_USER_AND_HOST="${TEST_USER}@${TEST_HOST}"
 
@@ -14,9 +17,9 @@ SERVER_PORT=7000
 
 servers_to_test=(
     go-tcp-echo-server \
+    rust-tokio-tcp-echo-server \
+    rust-threaded-tcp-echo-server \
 )
-#    rust-tokio-tcp-echo-server \
-#    rust-threaded-tcp-echo-server \
 
 for server_binary in "${servers_to_test[@]}"; do
 
@@ -40,8 +43,24 @@ for server_binary in "${servers_to_test[@]}"; do
         # Start server in background
         echo "Starting $server_binary on port ${SERVER_PORT}..."
         ssh $SSH_USER_AND_HOST "/tmp/$server_binary ${SERVER_PORT}" &
+
+        # Wait for server to start. Wait for port to be open
+        echo "Waiting for $server_binary to start..."
+        for i in {1..10}; do
+            if nc -z $TEST_HOST $SERVER_PORT; then
+                echo "$server_binary is up!"
+                break
+            else
+                echo "Waiting for $server_binary to start... ($i)"
+                sleep 1
+            fi
+        done
     
         # Run test
-        timeout 5s go run go-client/main.go ${TEST_HOST}:${SERVER_PORT} ${PARALLEL_CLIENTS}
+        timeout 5s go run ../go-client/main.go ${TEST_HOST}:${SERVER_PORT} ${PARALLEL_CLIENTS}
+
+        # Rest
+        echo Resting before next server test...
+        sleep 5
     )
 done
