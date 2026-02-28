0d4337be7e685da53d095306454d8029222988f8
Martin Nordholts <martin.nordholts@codetale.se>
2026-02-28 14:05:49 +0100

    wip  2026-02-28T14:05:49+01:00

--- servers/go-tcp-echo-server/build.sh
+++ servers/go-tcp-echo-server/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 output_path="$1"
 
-go build -o "$output_path" main.go
+go build -o "$output_path" "$SCRIPT_DIR/main.go"
