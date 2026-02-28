e78ae4149c804b215c23508cc9c5f70197fe0181
Martin Nordholts <martin.nordholts@codetale.se>
2026-02-28 14:10:20 +0100

    wip  2026-02-28T14:10:20+01:00

--- servers/deploy.sh
+++ servers/deploy.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BUILD_DIR="$SCRIPT_DIR/build"
+
 # First build and deploy all servers
 for SERVER in ./servers/*; do
     if [ ! -d "$SERVER" ]; then
         continue
     fi
-
     SERVER_NAME=$(basename $SERVER)
+
     echo "Building and deploying $SERVER_NAME..."
-    $SERVER/build.sh ./build/$SERVER_NAME
-    scp $USER@$HOST:./build/$SERVER_NAME $USER@$HOST:~/bin/$SERVER_NAME
+    $SERVER/build.sh "$BUILD_DIR/$SERVER_NAME"
+    scp "$BUILD_DIR/$SERVER_NAME" "$REMOTE_USER@$REMOTE_HOST:~/bin/$SERVER_NAME"
 done
