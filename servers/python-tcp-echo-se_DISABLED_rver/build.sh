486d92830486fc676767281bb2503c11746bf594
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-18 20:27:56 +0100

    wip  2026-03-18T20:27:56+01:00

--- servers/python-tcp-echo-se_DISABLED_rver/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-OUTPUT_PATH="$1"
-
-cp "$SCRIPT_DIR/main.py" "$OUTPUT_PATH"
-chmod +x "$OUTPUT_PATH"
